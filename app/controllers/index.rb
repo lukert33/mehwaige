162cf0378a8bb8636170c392b2c0e6d7b96e0876
Luke Thomas <lukert33@gmail.com>
2015-11-14 14:35:10 -0500

    Working email form; more copy.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,3 +10,30 @@ end
 get '/rsvp' do 
   erb :rsvp 
 end
+
+post '/rsvp' do
+  puts "zazz"
+  puts ENV['SENDGRID_USERNAME']
+ require 'pony'
+ Pony.mail({
+  :from => params[:name],
+    :to => ['lukert33@gmail.com', 'njsohl@gmail.com'],
+    :subject => params[:name] + " RSVP'd for the wedding",
+    :body => params[:comment],
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :domain => 'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  })
+  redirect '/success' 
+end
+
+get '/success' do
+  erb :success
+end
